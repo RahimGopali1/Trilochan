7dbdb5d9680bf13d410be29f78b39e928c568ae3
RahimGopali1 <grahim18@tbc.edu.np>
2023-12-21 14:42:39 +0545

    fixed navbar responsive dropdown and some pages responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
   if (window.innerWidth > 992) {
     document.querySelectorAll('.navbar .nav-item').forEach(function(everyitem) {
       everyitem.addEventListener('mouseover', function(e) {
-        let el_link = this.querySelector('a[data-mdb-toggle]');
+        let el_link = this.querySelector('a[data-bs-toggle]');
         if (el_link != null) {
           let nextEl = el_link.nextElementSibling;
           el_link.classList.add('show');
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
         }
       });
       everyitem.addEventListener('mouseleave', function(e) {
-        let el_link = this.querySelector('a[data-mdb-toggle]');
+        let el_link = this.querySelector('a[data-bs-toggle]');
         if (el_link != null) {
           let nextEl = el_link.nextElementSibling;
           el_link.classList.remove('show');
